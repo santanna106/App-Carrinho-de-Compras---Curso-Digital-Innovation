f0e3a409864f56680bfe2da744a883a87a722809
Gabriel Andrade <gabriel.santana@gmail.com>
2021-07-14 08:42:10 -0300

    Projeto finalizado

--- src/components/EmptyCart/index.js
+++ src/components/EmptyCart/index.js
@@ -1,12 +1,21 @@
 import React from 'react';
-import FeatherIcon from 'react-native-vector-icons/Feather'
+import LottieView from 'lottie-react-native';
+
+import emptyCartAnimation from '../../../EmptyCartAnimation.json'
 
 import * as S from './styles';
 
 export default function EmptyCart() {
  return (
    <S.Container>
-     <FeatherIcon name="slash" size={38} color="#f3f9ff" />
+     <S.EmptyCartContainer>
+       <LottieView
+        source={emptyCartAnimation}
+        resizeMode="contain"
+        autoPlay
+        loop
+       />
+     </S.EmptyCartContainer>
      <S.EmptyCartText>Seu carrinho está vazio</S.EmptyCartText>
    </S.Container>
   );
--- src/pages/Cart/index.js
+++ src/pages/Cart/index.js
@@ -27,6 +27,19 @@ export default function Cart() {
   return formatValue(cartAmount) ;
 },[products])
 
+function increment(product){
+  dispatch(CartActions.updateAmountRequest(product.id,product.amount + 1))
+}
+
+
+function decrement(product){
+  dispatch(CartActions.updateAmountRequest(product.id,product.amount - 1))
+}
+
+function removeFromCart(id){
+  dispatch(CartActions.removeFromCart(id));
+}
+
  return (
     <S.Container>
       <S.ProductContainer>
@@ -57,12 +70,12 @@ export default function Cart() {
               </S.ProductTitleContainer>
               <S.ActionContainer>
                 <S.ActionButton
-                  onPress={() => {}}
+                  onPress={() => increment(item)}
                 >
                   <FeatherIcon name="plus" color="#e83f5b" size={16} />
                 </S.ActionButton>
                 <S.ActionButton
-                  onPress={() => {}}
+                  onPress={() => item.amount > 1 ? decrement(item) : removeFromCart(item.id)}
                 >
                   <FeatherIcon name="minus" color="#e83f5b" size={16} />
                 </S.ActionButton>
